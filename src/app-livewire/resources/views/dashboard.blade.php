2f39a12ce0d6d0d2069c53267dae9ddb070cbd1c
m-suzuki <m-suzuki@networld.ne.jp>
2025-12-09 15:24:54 +0900

    install tallstackui

--- src/app-livewire/resources/views/dashboard.blade.php
+++ src/app-livewire/resources/views/dashboard.blade.php
@@ -1,18 +1,32 @@
-<x-layouts.app :title="__('Dashboard')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+<x-app-layout>
+    <x-card header="Welcome to the TallStackUI Starter Kit">
+        <div class="space-y-2">
+            <p>
+                👋🏻 This is the TallStackUI starter kit for Laravel 12. With this TallStackUI starter kit you will be able to enjoy a ready-to-use application to initialize your next Laravel 12 project with TallStackUI.
+            </p>
+            <div class="mt-4 space-y-2">
+                <i>
+                    "What this starter kit includes out of the box?"
+                </i>
+                <ul class="ml-2 mt-2 list-inside list-decimal font-semibold">
+                    <li>Laravel v12</li>
+                    <li>Livewire v3</li>
+                    <li>TallStackUI v2</li>
+                    <li>TailwindCSS v4</li>
+                </ul>
+                <p>And also:</p>
+                <ul class="ml-2 mt-2 list-inside list-decimal font-semibold">
+                    <li><a href="https://github.com/barryvdh/laravel-debugbar" target="_blank">DebugBar</a></li>
+                    <li><a href="https://github.com/larastan/larastan" target="_blank">LaraStan</a></li>
+                    <li><a href="https://pestphp.com/" target="_blank">Pest</a></li>
+                    <li><a href="https://laravel.com/docs/pint" target="_blank">Pint</a></li>
+                </ul>
             </div>
         </div>
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-        </div>
-    </div>
-</x-layouts.app>
+        <x-slot:footer>
+            <span class="text-xs">
+                ⚠️ <x-link href="https://tallstackui.com/docs/v2/starter-kit" bold blank sm>Make sure to read the docs about the starter kit!</x-link>
+            </span>
+        </x-slot:footer>
+    </x-card>
+</x-app-layout>
